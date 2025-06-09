1b15a111697534c3ae114063843b1741fcbb203c
David Hernández <50053027+adrojodev@users.noreply.github.com>
2025-06-08 21:56:59 -0600

    Organized shit

---
+++ src/content/index.ts
@@ -0,0 +1,22 @@
+export const posts = [
+  "Crear o consumir",
+  "Hoy decidí darle un rumbo a mí vida",
+  "Como organizo mis finanzas",
+  "Quiero más conexiones reales",
+  "La filosofía y la tecnología de este blog",
+];
+
+export const links = [
+  {
+    text: "Whatever happened to cheap eReaders?",
+    url: "https://shkspr.mobi/blog/2025/05/whatever-happened-to-cheap-ereaders/",
+  },
+  {
+    text: "Kurt Vonnegut's Wonderful Letter to a Group of High School Student (read by Ian McKellen)",
+    url: "https://www.youtube.com/watch?v=ypOY09JTvCo",
+  },
+  {
+    text: "Internet used to be a place",
+    url: "https://www.youtube.com/watch?v=oYlcUbLAFmw&t=3s",
+  },
+];
